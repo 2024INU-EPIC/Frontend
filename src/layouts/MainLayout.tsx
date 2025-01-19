2b50ad1e9cdfeb5e4af0fa23106b8535693fa1a4
dannyk20 <dannyk20@naver.com>
2025-01-19 22:23:55 +0900

    design: 메뉴바 퍼블리싱 및 메인 레이아웃 구성

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "../components/Header";
+
+type MainLayoutProps = {
+  children: React.ReactNode;
+};
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        backgroundColor: "#F0F0F0",
+      }}
+    >
+      <Header />
+      <main
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
