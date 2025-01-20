f542bedfb5ff245465a3322430ba5e643538e36c
seunggonhyeon <hyunsg5638@gmail.com>
2025-01-20 21:30:39 +0900

    design: Left Pane of LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Main from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </MainLayout>
     </Router>
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../assets/img/logo.svg"; // 로고 파일 경로
+import styled from "styled-components";
+
+const SplitScreen = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100wh;
+  justify-content: flex;
+  align-items: center;
+`;
+
+const LeftPane = styled.div`
+  // flex: 0 0 50.75rem; // 812px;
+  width: 42.29vw;
+  height: 100vh;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.75rem;
+  font-weight: 100;
+`;
+
+const RightPane = styled.div`
+  // flex: 0 0 69.25rem; // 1108px;
+  width: 57.71vw;
+  height: 100vh;
+  background-color: #ea5a47;
+  display: flex;
+  justify-content: flex;
+  align-items: center;
+`;
+
+const WelcomeLogo = styled.img`
+  width: 23.95vw; // 460px;
+`;
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <SplitScreen>
+        <LeftPane>
+          <WelcomeLogo src={Logo}></WelcomeLogo>
+          <h1>English Pronunciation Improvement Coach</h1>
+        </LeftPane>
+        <RightPane>
+          <p>Welcome back!</p>
+        </RightPane>
+      </SplitScreen>
+    </div>
+  );
+};
+
+export default LoginPage;
