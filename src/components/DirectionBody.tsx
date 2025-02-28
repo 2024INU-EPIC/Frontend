44d1ffff05c459f089e77aeb0754290b0b233588
dannyk20 <dannyk20@naver.com>
2025-02-28 21:58:01 +0900

    design: 모의고사 Part5 페이지, 결과 페이지 구현

---
+++ src/components/DirectionBody.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 87.5rem;
+  height: 25rem;
+  /* margin-top: 9.625rem; */
+  background-color: #ffffff;
+  border-radius: 1rem;
+  filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.25));
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p.question {
+    margin: 2.25rem 0 1.75rem 0;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  div {
+    width: 79rem;
+    display: flex;
+  }
+
+  p.paragraph {
+    position: relative;
+    font-size: 2rem;
+    font-weight: 400;
+    line-height: 3rem; // 48px
+    text-align: left;
+    /* display: inline-block; */
+  }
+`;
+
+type DirectionBodyProps = {
+  text: React.ReactNode;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const DirectionBody: React.FC<DirectionBodyProps> = ({
+  text,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <Wrapper>
+      <p className="question">
+        Question {questionNum} of {totalQuestions}
+      </p>
+      <div>
+        <p className="paragraph">{text}</p>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default DirectionBody;
---
+++ src/pages/Part5Page.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./Main.styled";
+import ScoreBody from "../components/ScoreBody";
+import ReplyBody from "../components/ReplyBody";
+import styled from "styled-components";
+import DirectionBody from "../components/DirectionBody";
+
+type TimeIndicatorProps = { bgColor?: string };
+// type TipProps = { text: string };
+
+export const TimeRemainingIndicator = styled.div<TimeIndicatorProps>`
+  margin-top: 6rem;
+  margin-bottom: 1.5rem;
+  width: 12.25rem;
+  height: 12.25rem;
+
+  border-radius: 50%;
+  border-color: black;
+
+  font-size: 2rem;
+  color: white;
+  filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.25));
+
+  // props에 따라 배경색 변경. true이면
+  background-color: ${(props) => props.bgColor || "#ff7b7b"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TimeInfoText = styled.p`
+  margin: 0;
+  font-size: 1.5rem;
+`;
+
+export const TopBlank = styled.div`
+  height: 9rem;
+`;
+
+const Part5Page: React.FC = () => {
+  const [isPreparing, setPreparing] = useState(true);
+  const [isResponding, setResponding] = useState(false);
+  const [isScoring, setScoring] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(45);
+
+  useEffect(() => {
+    if (remainingTime > 0) {
+      const timer = setTimeout(() => {
+        setRemainingTime(remainingTime - 1);
+      }, 1000); // 지연시간. 몇초마다 출력할 것인지. 현재 1초
+      return () => clearTimeout(timer);
+    } else {
+      if (isPreparing) {
+        setPreparing(false);
+        setResponding(true);
+        setRemainingTime(60);
+      } else if (isResponding) {
+        setResponding(false);
+        setScoring(true);
+      }
+    }
+  }, [remainingTime, isPreparing, isResponding]);
+
+  const textContent =
+    " The only way to reduce the amount of traffic in cities today is by reducing the need for people to travel from home for work, education or shopping. Do you agree or disagree with this point of view? Use specific reasons and examples to support your choice.";
+  const replyContent =
+    "The only way to reduce the amount of traffic in cities today is by reducing the need for people to travel from home for work, education or shopping. Do you agree or disagree with this point of view? Use specific reasons and examples to support your choice.The only way to reduce the amount of traffic in cities today is by reducing the need for people to travel from home for work, education or shopping. Do you agree or disagree with this point of view? Use specific reasons and examples to support your choice.The only way to reduce the amount of traffic in cities today is by reducing the need for people to travel from home for work, education or shopping. Do you agree or disagree with this point of view? Use specific reasons and examples to support your choice.";
+
+  // ? 이후 줄바꿈용 코드
+  const formattedText = textContent.split("?").map((part, index, arr) => (
+    <React.Fragment key={index}>
+      {part}
+      {index < arr.length - 1 && "?"}
+      {index === 0 && <br />}
+    </React.Fragment>
+  ));
+
+  return (
+    <S.mainContainer>
+      <TopBlank />
+      <DirectionBody
+        text={formattedText}
+        questionNum={11}
+        totalQuestions={11}
+      />
+      {isPreparing && (
+        <>
+          <TimeRemainingIndicator>
+            {`00 : ${remainingTime.toString().padStart(2, "0")}`}
+          </TimeRemainingIndicator>
+          <TimeInfoText>Preparation Time</TimeInfoText>
+        </>
+      )}
+      {isResponding && (
+        <>
+          <TimeRemainingIndicator bgColor="#59BED4">
+            {`00 : ${remainingTime.toString().padStart(2, "0")}`}
+          </TimeRemainingIndicator>
+          <TimeInfoText>Response Time</TimeInfoText>
+        </>
+      )}
+      {isScoring && (
+        <>
+          <ReplyBody text={replyContent} isScoring={isScoring} />
+          <ScoreBody
+            totalScore={86}
+            accuracy={80}
+            completeness={60}
+            fluency={85}
+            prosody={70}
+          />
+        </>
+      )}
+    </S.mainContainer>
+  );
+};
+
+export default Part5Page;
